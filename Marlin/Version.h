StefanIGit/Marlin_BigBox_Dual

BigBox Hybrid Titan settings

Marlin/Version.h
@@ -29,13 +29,13 @@
 /**
  * Marlin release version identifier
  */
-#define SHORT_BUILD_VERSION "1.1.0-RC6"
+#define SHORT_BUILD_VERSION "1.1.0-RC6 BBDual"
 
 /**
  * Verbose version identifier which should contain a reference to the location
  * from where the binary was downloaded or the source code was compiled.
  */
-#define DETAILED_BUILD_VERSION SHORT_BUILD_VERSION " (Github)"
+#define DETAILED_BUILD_VERSION SHORT_BUILD_VERSION "1.1.0-RC6 BigBox Dual"
 
 /**
  * The STRING_DISTRIBUTION_DATE represents when the binary file was built,
@@ -52,7 +52,7 @@
 /**
  * Defines a generic printer name to be output to the LCD after booting Marlin.
  */
-#define MACHINE_NAME "3D Printer"
+#define MACHINE_NAME "BigBox Dual"
 
 /**
  * The SOURCE_CODE_URL is the location where users will find the Marlin Source
@@ -60,7 +60,7 @@
  * has a distinct Github fork— the Source Code URL should just be the main
  * Marlin repository.
  */
-#define SOURCE_CODE_URL "https://github.com/MarlinFirmware/Marlin"
+#define SOURCE_CODE_URL "https://github.com/Alex9779/Marlin_BigBox_Dual"
 
 /**
  * Default generic printer UUID.

mosh1/Marlin

Marlin/Version.h
@@ -29,13 +29,13 @@
 /**
  * Marlin release version identifier
  */
-#define SHORT_BUILD_VERSION "1.1.0-RC6"
+#define SHORT_BUILD_VERSION "1.1.0-RC6 BBPro"
 
 /**
  * Verbose version identifier which should contain a reference to the location
  * from where the binary was downloaded or the source code was compiled.
  */
-#define DETAILED_BUILD_VERSION SHORT_BUILD_VERSION " (Github)"
+#define DETAILED_BUILD_VERSION SHORT_BUILD_VERSION "1.1.0-RC6 BigBox Pro"
 
 /**
  * The STRING_DISTRIBUTION_DATE represents when the binary file was built,
@@ -52,7 +52,7 @@
 /**
  * Defines a generic printer name to be output to the LCD after booting Marlin.
  */
-#define MACHINE_NAME "3D Printer"
+#define MACHINE_NAME "BigBox Pro"
 
 /**
  * The SOURCE_CODE_URL is the location where users will find the Marlin Source
@@ -60,7 +60,7 @@
  * has a distinct Github fork— the Source Code URL should just be the main
  * Marlin repository.
  */
-#define SOURCE_CODE_URL "https://github.com/MarlinFirmware/Marlin"
+#define SOURCE_CODE_URL "https://github.com/Alex9779/Marlin"
 
 /**
  * Default generic printer UUID.