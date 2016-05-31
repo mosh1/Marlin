f6a8d473e99bdc3f430a65d2531032626b73d12d
Alexander Leisentritt <alexander.leisentritt@alitecs.de>
2016-06-01 00:37:10 +0200

    adjusted mesh points

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -451,10 +451,10 @@
 // Below are the macros that are used to define the borders for the mesh area,
 // made available here for specialized needs, ie dual extruder setup.
 #if ENABLED(MESH_BED_LEVELING)
-  #define MESH_MIN_X (X_MIN_POS + MESH_INSET)
-  #define MESH_MAX_X (X_MAX_POS - (MESH_INSET))
-  #define MESH_MIN_Y (Y_MIN_POS + MESH_INSET)
-  #define MESH_MAX_Y (Y_MAX_POS - (MESH_INSET))
+  #define MESH_MIN_X 15
+  #define MESH_MAX_X 285
+  #define MESH_MIN_Y 15
+  #define MESH_MAX_Y 185
 #endif
 
 // @section extras
