3d49e091fb2cbdf1a6d1e5a166292baa2c650c5e
Moshen Chan <moshen@gmail.com>
2016-12-01 12:47:13 -0800

    z bump optimize

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -762,7 +762,7 @@
   // Gradually reduce leveling correction until a set height is reached,
   // at which point movement will be level to the machine's XY plane.
   // The height can be set with M420 Z<height>
-  #define ENABLE_LEVELING_FADE_HEIGHT
+  // #define ENABLE_LEVELING_FADE_HEIGHT
 
 #endif  // MESH_BED_LEVELING
 
